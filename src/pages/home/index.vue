e9d063d82b00757aaa6a66cedf70e1ba98e9e204
renlingxin <51167592+renlingxin@users.noreply.github.com>
2021-07-25 23:07:49 +0800

    feat:初始化ts

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,14 +1,15 @@
 <template>
-  <Home msg="Welcome to Your Vue.js + TypeScript App"/>
+<div>
+  home
+</div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import Home from './pages/home/index.vue';
+
 
 @Options({
   components: {
-    Home,
   },
 })
 export default class App extends Vue {}
--- src/pages/preview/index.vue
+++ src/pages/preview/index.vue
@@ -1,14 +1,14 @@
 <template>
-  <Home msg="Welcome to Your Vue.js + TypeScript App"/>
+
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import Home from './pages/home/index.vue';
+
 
 @Options({
   components: {
-    Home,
+
   },
 })
 export default class App extends Vue {}
